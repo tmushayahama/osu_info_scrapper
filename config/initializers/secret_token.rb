5efa3f6e25a3abb123d21cfabdc1dab0dc35f21a
tmushayahama <tmushayahama@gmail.com>
2014-03-06 17:50:27 -0500

    Initialized the ruby project and the setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OsuInfoScrapper::Application.config.secret_key_base = '15e74d42ff88fb116ec45f09bb1270f01fa7d033e592f69e93eb580359ac4326be8edbd01bfb633cd251c8d676cf874ac8447416b52a001d75193d68953994ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OsuInfoScrapper::Application.config.session_store :cookie_store, key: '_osu_info_scrapper_session'
